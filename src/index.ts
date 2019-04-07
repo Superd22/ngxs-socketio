2134bb08b3e4198b9418da631d21599800211ab3
David <dfain@wemaintain.io>
2019-04-07 17:59:13 +0200

    build: gitlab ci & semver

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 export { NgxsSocketIOPluginModule } from './lib/socket-io.module';
 export {
-  NgxsSocketIOPluginOptions,
-  NGXS_SOCKETIO_OPTIONS,
-  ConnectSocketIO as ConnectWebSocket,
-  SocketIOWebSocket as DisconnectWebSocket,
-  SocketIOConnected as WebSocketConnected,
-  SocketIODisconnected as WebSocketDisconnected,
-  AuthenticateSocketIO as AuthenticateWebSocket,
-  SendSocketIOAction as SendWebSocketAction,
-  SocketIOMessageError as WebsocketMessageError,
+    NgxsSocketIOPluginOptions,
+    NGXS_SOCKETIO_OPTIONS,
+    ConnectSocketIO as ConnectWebSocket,
+    SocketIOWebSocket as DisconnectWebSocket,
+    SocketIOConnected as WebSocketConnected,
+    SocketIODisconnected as WebSocketDisconnected,
+    AuthenticateSocketIO as AuthenticateWebSocket,
+    SendSocketIOAction as SendWebSocketAction,
+    SocketIOMessageError as WebsocketMessageError,
 } from './lib/symbols';
